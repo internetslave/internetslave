988e23ec8a6b276821ce15e1fd4120263517ed12
internetslave <80433265+internetslave@users.noreply.github.com>
2023-08-19 12:49:42 -0400

    Space between left logo / right button
    Made logo into flex container itself so 3 items would horizontally align
    
    Create space between left flex container and right flex item.
    justify-content: space-between;
    ( parent header container)

---
+++ Script/index.js
@@ -0,0 +1,31 @@
+
+//click event listener to button 
+const btn = document.getElementById('btn');
+
+// ✅ Toggle button text on click
+
+btn.addEventListener('click', function handleClick() 
+{
+  // each time the button is clicked check if the text is the initial text
+  const initialText = 'Philosophy';
+
+  // each time the philosophy button is clicked though it should change the body
+  // of the index html page (however the header stuff remains the same)
+
+  // if the initialtext matches change to clicked text
+  if (btn.innerHTML.toLowerCase().includes(initialText.toLowerCase()))
+    {
+        btn.innerHTML = 'Reflections';
+    } 
+
+  // otherwise change it back to the initialtext
+  else
+  {
+    btn.innerHTML = initialText;
+  }
+});
+
+/**
+ * ✅ If you need to change the button's inner HTML use:
+ *  - `innerHTML` instead of `textContent`
+ */
