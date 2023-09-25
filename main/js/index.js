bb4b4b736d4ddb3092e5700b4bbb092ff0e081e6
internetslave <80433265+internetslave@users.noreply.github.com>
2023-09-25 16:29:39 +0100

    h1 - toggles

--- main/js/index.js
+++ main/js/index.js
@@ -1,34 +1,47 @@
+// https://bobbyhadz.com/blog/javascript-change-button-text-on-click
 
-// site used - https://bobbyhadz.com/blog/javascript-change-button-text-on-click
-// Toggle button text on Click using JavaScript
+
+// forward definitions for fucntion
+function updateThenDisplayPhilosophy()
+{
+  let text = document.getElementById("h1").innerHTML;
+  document.getElementById("h1").innerHTML = text.replace("Why I reflect", "My Values");
+
+  
+
+}
+
+function updateThenDisplayReflections()
+{
+  let text = document.getElementById("h1").innerHTML;
+  document.getElementById("h1").innerHTML = text.replace("My Values", "Why I reflect");
+}
+
 
 //click event listener to button 
 const btn = document.getElementById('btn');
 
-// ✅ Toggle button text on click
+// toggle button text on click
 
+//inbuilt javascript function
 btn.addEventListener('click', function handleClick() 
 {
   // each time the button is clicked check if the text is the initial text
   const initialText = 'Philosophy';
 
-  // each time the philosophy button is clicked though it should change the body
-  // of the index html page (however the header stuff remains the same)
-
-  // if the initialtext matches change to clicked text
-  if (btn.innerHTML.toLowerCase().includes(initialText.toLowerCase()))
+  // if the initialtext matches change to clicked text (if Philosophy (once clicked) --> change to Reflections)
+  if (btn.textContent.toLowerCase().includes(initialText.toLowerCase()))
     {
-        btn.innerHTML = 'Reflections';
+      let text = updateThenDisplayPhilosophy(); // calling function - invoking it through it's name
+      btn.textContent = 'Reflections'; 
     } 
 
   // otherwise change it back to the initialtext
   else
   {
-    btn.innerHTML = initialText;
+    let text = updateThenDisplayReflections(); // calling function - invoking it through it's name
+    btn.textContent = initialText;
   }
-});
 
-/**
- * ✅ If you need to change the button's inner HTML use:
- *  - `innerHTML` instead of `textContent`
- */
+ 
+});
