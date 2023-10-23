4d215e944733bc7a6aecb3c7623eda2899bf2dfa
internetslave <flyingpig49@protonmail.com>
2023-10-23 09:38:26 +0100

    key value pair for sections

--- js/index.js
+++ js/index.js
@@ -77,14 +77,87 @@ renderButtons = () => {
       selectHeader(opt);
 
       const sections = {
-        "Justification": "Justification text.",
-        "Goals": "Goals text.",
-        "Description": "Description text.",
-        "Lessons Learned": "Lessons learned text.",
+        "Description": {
+          // location path name(not relative path) - string object property name
+          // object property name must match the location path name for the page calling the function
+          "/reflection-subpages/cert-mentor.html" : "Description text for cert.",
+          "/reflection-subpages/code-dojo.html": "Description text for code.",
+          "/reflection-subpages/course-rep.html": "Description text for course.",
+          "/reflection-subpages/grads4nottm.html": "Description text for grads.",
+          "/reflection-subpages/it-support.html": "Description text for it.",
+          "/reflection-subpages/library-student-mentor.html": "Description text for library.",
+          "/reflection-subpages/outreach-ambassador.html": "Description text for outreach.",
+          "/reflection-subpages/this-website.html": "Description text for website.",
+          "/reflection-subpages/trinity.html": "Description text for trinity.",
+
+          "/tools-subpages/arch.html": "Description text for arch.",
+          "/tools-subpages/graphene.html": "Description text for graph.",
+          "/tools-subpages/monero.html": "Description text for xmr.",
+          "/tools-subpages/pi-hole.html": "Description text for pi.",
+
+
+        },
+        "Justification": {
+          "/reflection-subpages/cert-mentor.html": "Justification text for cert.",
+          "/reflection-subpages/code-dojo.html": "Justification text for code.",
+          "/reflection-subpages/course-rep.html": "Justification text for course.",
+          "/reflection-subpages/grads4nottm.html": "Justification text for grads.",
+          "/reflection-subpages/it-support.html": "Justification text for it.",
+          "/reflection-subpages/library-student-mentor.html": "Justification text for library.",
+          "/reflection-subpages/outreach-ambassador.html": "Justification text for outreach.",
+          "/reflection-subpages/this-website.html": "Justification text for website.",
+          "/reflection-subpages/trinity.html": "Justification text for trinity.",
+
+
+          "/tools-subpages/arch.html": "Justification text for arch.",
+          "/tools-subpages/graphene.html": "Justification text for graph.",
+          "/tools-subpages/monero.html": "Justification text for xmr.",
+          "/tools-subpages/pi-hole.html": "Justification text for pi.",
+
+        },
+
+        "Lessons Learned": {
+          "/reflection-subpages/cert-mentor.html": "Lessons text for cert.",
+          "/reflection-subpages/code-dojo.html": "Lessons text for code.",
+          "/reflection-subpages/course-rep.html": "Lessons text for course.",
+          "/reflection-subpages/grads4nottm.html": "Lessons text for grads.",
+          "/reflection-subpages/it-support.html": "Lessons text for it.",
+          "/reflection-subpages/library-student-mentor.html": "Lessons text for library.",
+          "/reflection-subpages/outreach-ambassador.html": "Lessons text for outreach.",
+          "/reflection-subpages/this-website.html": "Lesssons text for website.",
+          "/reflection-subpages/trinity.html": "Lessons text for trinity.",
+
+
+          "/tools-subpages/arch.html": "Lessons text for arch.",
+          "/tools-subpages/graphene.html": "Lessons text for graph.",
+          "/tools-subpages/monero.html": "Lessons text for xmr.",
+          "/tools-subpages/pi-hole.html": "Lessons text for pi.",
+        },
+
+        "Goals": {
+          "/reflection-subpages/cert-mentor.html": "Goals text for cert.",
+          "/reflection-subpages/code-dojo.html": "Goals text for code.",
+          "/reflection-subpages/course-rep.html": "Goals text for course.",
+          "/reflection-subpages/grads4nottm.html": "Goals text for grads.",
+          "/reflection-subpages/it-support.html": "Goals text for it.",
+          "/reflection-subpages/library-student-mentor.html": "Goals text for library.",
+          "/reflection-subpages/outreach-ambassador.html": "Goals text for outreach.",
+          "/reflection-subpages/this-website.html": "Goals text for website.",
+          "/reflection-subpages/trinity.html": "Goals text for trinity.",
+
+
+          "/tools-subpages/arch.html": "Goals text for arch.",
+          "/tools-subpages/graphene.html": "Goals text for graph.",
+          "/tools-subpages/monero.html": "Goals text for xmr.",
+          "/tools-subpages/pi-hole.html": "Goals text for pi.",
+
+        },
+
       };
 
+      updatePara(sections[currentOption][location.pathname]  || sections["Description"][location.pathname] 
+      );
 
-      updatePara(sections[currentOption] || sections["Description"]);
     });
 
 		container.append(subpageBtn); // elements appear where you append them
